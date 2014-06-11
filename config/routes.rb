640c45bf8923a6fa02165c9c2fb3b081f66679e8
Thomas Berry <thom.berry@gmail.com>
2014-06-11 19:26:17 -0400

    setting up comfy mexi sofa

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  comfy_route :cms_admin, :path => '/admin'
+
+  # Make sure this routeset is defined last
+  comfy_route :cms, :path => '/', :sitemap => false
+
   devise_for :users
   
   root 'welcome#index'
