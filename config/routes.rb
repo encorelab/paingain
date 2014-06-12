1a1739b77e1664d0c766f9354d8604c710c2e745
Thomas Berry <thom.berry@gmail.com>
2014-06-11 21:15:50 -0400

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   root 'welcome#index'
 
+  devise_for :users
+
   comfy_route :cms_admin, :path => '/admin'
 
   # Make sure this routeset is defined last
   comfy_route :cms, :path => '/', :sitemap => false
-
-  devise_for :users
 
 
 
