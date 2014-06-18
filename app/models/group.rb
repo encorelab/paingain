e7b1a2efea253ef99ab7d39d7615830093f7eb67
Thomas Berry <thom.berry@gmail.com>
2014-06-18 18:51:09 -0400

    added role to user and validations

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,8 @@
 class Group < ActiveRecord::Base
+  has_many :users
+
+  validates_uniqueness_of :name
+  validates_presence_of :name
+  validates :name, length: { in: 4..20 }
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,11 @@
 class User < ActiveRecord::Base
+  belongs_to :group
+  validates_presence_of :group
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :authentication_keys => [:login]
   attr_accessor :login
-
-  validates_uniqueness_of :group_name
-  validates_presence_of :group_name
-  validates :group_name, length: { in: 4..20 }
 
   def self.find_first_by_auth_conditions(warden_conditions)
     conditions = warden_conditions.dup
