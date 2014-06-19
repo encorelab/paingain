61c112f45a7a80db37a58046d3a21382d38f4cde
Thomas Berry <thom.berry@gmail.com>
2014-06-19 11:13:41 -0400

    removed abort message

--- lib/paingain/admin_authentication.rb
+++ lib/paingain/admin_authentication.rb
@@ -1,7 +1,6 @@
 module Paingain::AdminAuthentication
   def authenticate
     unless user_signed_in? && current_user.role == 'admin'
-      about 'here'
       flash[:danger] = 'You must be an admin.'
       redirect_to new_user_session_path
     end
