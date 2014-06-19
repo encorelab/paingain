b338cd36f0ea839b320f4c259f2268e273c44c84
Thomas Berry <thom.berry@gmail.com>
2014-06-18 23:42:26 -0400

    admin group and user view with minimal controllers

--- app/controllers/admin/groups_controller.rb
+++ app/controllers/admin/groups_controller.rb
@@ -1,4 +1,20 @@
 class Admin::GroupsController < Comfy::Admin::Cms::BaseController
   def index
+    @groups = Group.all
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destory
   end
 end
--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,20 @@
 class Admin::UsersController < Comfy::Admin::Cms::BaseController
   def index
+    @users = User.all
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destory
   end
 end
